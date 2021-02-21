0cbce4aed6b6075aec87e65a77218d2433598070
Jonathan Lin <ernsheong@gmail.com>
2021-02-21 16:32:36 +0800

    Initial error checking in JSON editor

--- src/components/bt-json-editor.js
+++ src/components/bt-json-editor.js
@@ -3,23 +3,28 @@ import { html, css } from "lit-element";
 import CodeFlask from "codeflask";
 
 import BTBase from "../bt-base";
+import { isEmpty } from "lodash";
+
+const ErrorInvalidJSON = "error_invalid_json";
 
 class BTJSONEditor extends BTBase {
   static get properties() {
     return {
       model: { type: String },
+      readonly: { type: Boolean },
+
+      _errors: { type: Array },
     };
   }
 
   constructor() {
     super();
+
+    this._errors = [];
   }
 
   render() {
-    return html`
-      <div id="editor"></div>
-      <div>Foo</div>
-    `;
+    return html` <div id="editor"></div> `;
   }
 
   firstUpdated() {
@@ -32,6 +37,8 @@ class BTJSONEditor extends BTBase {
     });
 
     this._flask.onUpdate((code) => {
+      this.model = code;
+      this.validate();
       this._emit("model-change", { value: code });
     });
   }
@@ -40,6 +47,31 @@ class BTJSONEditor extends BTBase {
     if (changed.has("model")) {
       this._flask.updateCode(this.model);
     }
+
+    if (changed.has("readonly")) {
+      if (this.readonly) {
+        this._flask.enableReadonlyMode();
+      } else {
+        this._flask.disableReadonlyMode();
+      }
+    }
+  }
+
+  validate() {
+    const errors = [];
+    try {
+      const json = JSON.parse(this.model);
+      const isValid = typeof json === "object" && !isEmpty(json);
+      if (!isValid) {
+        errors.push(ErrorInvalidJSON);
+      }
+    } catch (e) {
+      errors.push(ErrorInvalidJSON);
+    }
+
+    this._errors = errors;
+
+    return errors.length === 0;
   }
 
   static get styles() {
